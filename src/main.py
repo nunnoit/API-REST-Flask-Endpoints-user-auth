6b34608d4b19b0a3852e9917675164278e55fc94
nunnoit <devit@tuta.io>
2022-10-25 23:57:38 +0000

    push code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,9 +5,10 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, People, Favorite_People
-#from models import Person
+from models import db, User, People, Favorite_People, Planets, Favorite_Planets, Vehicles, Favorite_Vehicles
 
+
+#Settings
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -30,79 +28,369 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# Endpoint get all users
 @app.route('/user', methods=['GET'])
 def handle_hello():
     users = User.query.all()
     #print(users)
-    users = list(map( lambda user: user.serialize(), users)) #(user)=>user.serialize()
+    # (user)=>user.serialize()
+    users = list(map(lambda user: user.serialize(), users))
     #print(users)
     response_body = {
         "msg": "Hello, this is your GET /user response "
-    }   
+    }
     return jsonify(users), 200
 
+# Endpoint post user
 @app.route('/user', methods=['POST'])
 def create_new_user():
     body = request.get_json()
-    #print(body['username']) 
-    descripcion=""
+    #print(body['username'])
+    descripcion = ""
 
     if body is None or "email" not in body:
-        raise APIException("Body está vacío o email no viene en el body, es inválido" , status_code=400)
-    if body['email'] is None or body['email']=="":
-        raise APIException("email es inválido" , status_code=400)
-    if body['password'] is None or body['password']=="":
-        raise APIException("password es inválido" , status_code=400)
-    if body['description'] is None or body['description']=="":
-        descripcion="no hay descripción"
+        raise APIException(
+            "Body está vacío o email no viene en el body, es inválido", status_code=400)
+    if body['email'] is None or body['email'] == "":
+        raise APIException("email es inválido", status_code=400)
+    if body['password'] is None or body['password'] == "":
+        raise APIException("password es inválido", status_code=400)
+    if body['description'] is None or body['description'] == "":
+        descripcion = "no hay descripción"
     else:
-        descripcion=body['description']
+        descripcion = body['description']
 
-    new_user = User(email=body['email'], password=body['password'], is_active=True, description=descripcion)
+    new_user = User(email=body['email'], password=body['password'],
+                    is_active=True, description=descripcion)
     users = User.query.all()
-    users = list(map( lambda user: user.serialize(), users))
+    users = list(map(lambda user: user.serialize(), users))
 
     for i in range(len(users)):
-        if(users[i]['email']==new_user.serialize()['email']):
-            raise APIException("El usuario ya existe" , status_code=400)
-            
+        if (users[i]['email'] == new_user.serialize()['email']):
+            raise APIException("El usuario ya existe", status_code=400)
+
     print(new_user)
     #print(new_user.serialize())
-    db.session.add(new_user) 
+    db.session.add(new_user)
     db.session.commit()
-    
-    return jsonify({"mensaje": "Usuario creado exitósamente"}), 201
 
+    return jsonify({"mensaje": "Usuario creado exitosamente"}), 201
+
+# Endpoint get user by id
 @app.route('/user/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
-    if user_id==0:
-        raise APIException("Id no puede ser igual a 0", status_code=400)  
+    if user_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
     user = User.query.get(user_id)
     if user == None:
-        raise APIException("El usuario no existe", status_code=400)  
+        raise APIException("El usuario no existe", status_code=400)
     #print(user.serialize())
     return jsonify(user.serialize()), 200
 
+# Endpoint delete user by id
 @app.route('/user/<int:user_id>', methods=['DELETE'])
 def delete_user_by_id(user_id):
-    if user_id==0:
-        raise APIException("Id no puede ser igual a 0", status_code=400)  
+    if user_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
     user = User.query.get(user_id)
     if user == None:
-        raise APIException("El usuario no existe", status_code=400)  
+        raise APIException("El usuario no existe", status_code=400)
     #print(user.serialize())
     db.session.delete(user)
     db.session.commit()
-    return jsonify("usuario eliminado exitósamente"), 200
+    return jsonify("usuario eliminado exitosamente"), 200
 
+# Endpoint get all people
 @app.route('/people', methods=['GET'])
 def get_people():
     peoples = People.query.all()
     #print(users)
-    peoples = list(map( lambda people: people.serialize(), peoples)) 
-    #print(users)  
+    peoples = list(map(lambda people: people.serialize(), peoples))
+    #print(users)
     return jsonify(peoples), 200
 
+# Endpoint get people by id
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_by_id(people_id):
+    if people_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    person = People.query.get(people_id)
+    if person == None:
+        raise APIException("El usuario no existe", status_code=400)
+    return jsonify(person.serialize()), 200
+
+# Endpoint get all planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+    return jsonify(planets), 200
+
+# Endpoint get planet by id
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    if planet_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    planet = Planets.query.get(planet_id)
+    if planet == None:
+        raise APIException("El planeta no existe", status_code=400)
+    return jsonify(planet.serialize()), 200
+
+# Endpoint get all vehicles
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicles.query.all()
+    vehicles = list(map(lambda vehicle: vehicle.serialize(), vehicles))
+    return jsonify(vehicles), 200
+
+# Endpoint get vehicle by id
+@app.route('/vehicle/<int:vehicle_id>', methods=['GET'])
+def get_vehicle_by_id(vehicle_id):
+    if vehicle_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    vehicle = Vehicles.query.get(vehicle_id)
+    if vehicle == None:
+        raise APIException("El vehículo no existe", status_code=400)
+    return jsonify(vehicle.serialize()), 200
+
+#Endpoint get all favorites from user
+@app.route('/user/favorites', methods=['GET'])
+def get_favorites():
+    favorite_peoples = Favorite_People.query.all()
+    favorite_peoples = list(
+        map(lambda favorite_people: favorite_people.serialize(), favorite_peoples))
+    favorite_planets = Favorite_Planets.query.all()
+    favorite_planets = list(
+        map(lambda favorite_planet: favorite_planet.serialize(), favorite_planets))
+    favorite_vehicles = Favorite_Planets.query.all()
+    favorite_vehicles = list(
+        map(lambda favorite_vehicle: favorite_vehicle.serialize(), favorite_vehicles))
+    favorites_list = favorite_peoples + favorite_planets + favorite_vehicles
+    print(favorites_list)
+    return jsonify(favorites_list), 200
+
+# Endpoint post people
+@app.route('/people', methods=['POST'])
+def create_new_person():
+    body = request.get_json()
+    # Validations
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    if body['name'] is None or body['name'] == "":
+        raise APIException("name es inválido", status_code=400)
+
+    new_character = People(name=body['name'], height=body['height'], mass=body['mass'], hair_color=body['hair_color'], skin_color=body['skin_color'],
+                           eye_color=body['eye_color'], birth_year=body['birth_year'], gender=body['gender'], homeworld=body['homeworld'])
+    characters = People.query.all()
+    characters = list(map(lambda character: character.serialize(), characters))
+
+    # for i in range(len(characters)):
+    #     if(characters[i]['name']==new_character.serialize()['name']):
+    #         raise APIException("El personaje ya existe" , status_code=400)
+
+    print(new_character)
+    #print(new_user.serialize())
+    db.session.add(new_character)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Personaje creado exitosamente"}), 201
+
+# Endpoint post planet
+@app.route('/planet', methods=['POST'])
+def create_new_planet():
+    body = request.get_json()
+    # Validations
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    if body['name'] is None or body['name'] == "":
+        raise APIException("name es inválido", status_code=400)
+
+    new_planets = Planets(name=body['name'], diameter=body['diameter'], rotation_Period=body['rotation_Period'], orbital_Period=body['orbital_Period'],
+                          gravity=body['gravity'], population=body['population'], climate=body['climate'], terrain=body['terrain'], surface_Water=body['surface_Water'])
+    planets = Planets.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+
+    for i in range(len(planets)):
+        if (planets[i]['name'] == new_planets.serialize()['name']):
+            raise APIException("El planeta ya existe", status_code=400)
+
+    print(new_planets)
+    #print(new_user.serialize())
+    db.session.add(new_planets)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Planeta creado exitosamente"}), 201
+
+# Endpoint post vehicle
+@app.route('/vehicle', methods=['POST'])
+def create_new_vehicle():
+    body = request.get_json()
+    # Validations
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    if body['name'] is None or body['name'] == "":
+        raise APIException("name es inválido", status_code=400)
+
+    new_vehicles = Vehicles(name=body['name'], model=body['model'], vehicle_class=body['vehicle_class'], manufacturer=body['manufacturer'], cost_in_credits=body['cost_in_credits'], length=body['length'],
+                            crew=body['crew'], passengers=body['passengers'], max_atmosphering_speed=body['max_atmosphering_speed'], cargo_capacity=body['cargo_capacity'], consumables=body['consumables'])
+    vehicles = Vehicles.query.all()
+    vehicles = list(map(lambda vehicle: vehicle.serialize(), vehicles))
+
+    for i in range(len(vehicles)):
+        if (vehicles[i]['name'] == new_vehicles.serialize()['name']):
+            raise APIException("El vehículo ya existe", status_code=400)
+
+    print(new_vehicles)
+    db.session.add(new_vehicles)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Vehículo creado exitosamente"}), 201
+
+# Endpoint delete people by id
+@app.route('/people/<int:item_id>', methods=['DELETE'])
+def delete_character_by_id(item_id):
+    if item_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    character = People.query.get(item_id)
+    if character == None:
+        raise APIException("El personaje no existe", status_code=400)
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify("personaje eliminado exitosamente"), 200
+
+# Endpoint delete planet by id
+@app.route('/planet/<int:item_id>', methods=['DELETE'])
+def delete_planet_by_id(item_id):
+    if item_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    planet = Planets.query.get(item_id)
+    if planet == None:
+        raise APIException("El planeta no existe", status_code=400)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify("planeta eliminado exitosamente"), 200
+
+# Endpoint vehicle planet by id
+@app.route('/vehicle/<int:item_id>', methods=['DELETE'])
+def delete_vehicle_by_id(item_id):
+    if item_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    vehicle = Vehicles.query.get(item_id)
+    if vehicle == None:
+        raise APIException("El vehículo no existe", status_code=400)
+    db.session.delete(vehicle)
+    db.session.commit()
+    return jsonify("vehículo eliminado exitosamente"), 200
+
+# Endpoint DELETE drom FAVORITE people
+@app.route('/favorites/people/<int:item_id>', methods=['DELETE'])
+def delete_favorite_character_by_id(item_id):
+    if item_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    item = People.query.get(item_id)
+    if item == None:
+        raise APIException("El personaje no existe", status_code=400)
+    db.session.delete(item)
+    db.session.commit()
+    return jsonify("Personaje eliminado exitosamente"), 200
+
+# Endpoint DELETE drom FAVORITE planet
+@app.route('/favorites/planet/<int:item_id>', methods=['DELETE'])
+def delete_favorite_planet_by_id(item_id):
+    if item_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    item = Planets.query.get(item_id)
+    if item == None:
+        raise APIException("El planeta no existe", status_code=400)
+    db.session.delete(item)
+    db.session.commit()
+    return jsonify("Planeta eliminado exitosamente"), 200
+
+# Endpoint DELETE drom FAVORITE vehicle
+@app.route('/favorites/vehicle/<int:item_id>', methods=['DELETE'])
+def delete_favorite_vehicle_by_id(item_id):
+    if item_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    item = Vehicles.query.get(item_id)
+    if item == None:
+        raise APIException("El vehículo no existe", status_code=400)
+    db.session.delete(item)
+    db.session.commit()
+    return jsonify("Vehículo eliminado exitosamente"), 200
+
+
+#Endpoint Update people
+@app.route('/people/<int:people_id>', methods=['PUT'])
+def put_people_by_id(people_id):
+    if people_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    person = People.query.get(people_id)  # Search by id
+    if person == None:
+        raise APIException("El usuario no existe", status_code=400)
+    body = request.get_json()
+    #Validation
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    # Check for body if empty (request)
+    if not body['name'] is None:
+        person.name = body['name']
+    db.session.commit()
+    return jsonify(person.serialize()), 200
+
+#Endpoint Update planet
+@app.route('/planet/<int:planet_id>', methods=['PUT'])
+def put_planet_by_id(planet_id):
+    if planet_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    planet = Planets.query.get(planet_id)  # Search by id
+    if planet == None:
+        raise APIException("El planeta no existe", status_code=400)
+    body = request.get_json()
+    #Validation
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    # Check for body if empty (request)
+    if not body['name'] is None:
+        planet.name = body['name']
+    db.session.commit()
+    return jsonify(planet.serialize()), 200
+
+#Endpoint Update vehicle
+@app.route('/vehicle/<int:vehicle_id>', methods=['PUT'])
+def put_vehicle_by_id(vehicle_id):
+    if vehicle_id == 0:
+        raise APIException("Id no puede ser igual a 0", status_code=400)
+    vehicle = Vehicles.query.get(vehicle_id)  # Search by id
+    if vehicle == None:
+        raise APIException("El vehiculo no existe", status_code=400)
+    body = request.get_json()
+    #Validation
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    # Check for body if empty (request)
+    if not body['name'] is None:
+        vehicle.name = body['name']
+    db.session.commit()
+    return jsonify(vehicle.serialize()), 200
+
+#Endpoint Search people
+@app.route('/people/busqueda', methods=['POST'])
+def busqueda_people():
+    body = request.get_json()
+    #Validation
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    if not body['name'] is None:
+        # va a encontrar todas las coincidencias
+        found = People.query.filter(People.name == body['name']).all()
+        found = list(map(lambda item: item.serialize(), found))
+        print(found)
+    if found == None:
+        raise APIException("El personaje no existe", status_code=400)
+    return jsonify(found), 200
+
+
+# <THE END/>
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -8,7 +8,11 @@ class User(db.Model):
     password = db.Column(db.String(250), unique=False, nullable=False) 
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     description = db.Column(db.String(120), unique=False, nullable=False)
+    #conectando la tabla user con las tablas pivote de planets, vehicles y people 
     user_favorite = db.relationship("Favorite_People", backref="user")
+    user_favorite_planet = db.relationship("Favorite_Planets", backref="user")
+    user_favorite_vehicles = db.relationship("Favorite_Vehicles", backref="user")
+
 
     #de manera automática, el nombre de la tabla es el nombre de la clase en minúscula
     def __repr__(self):
@@ -20,31 +24,138 @@ class User(db.Model):
             "email": self.email,
             "is_active": self.is_active,
             "descripcion": self.description
-            # do not serialize the password, its a security breach
         }
 
+# Tabla Characters
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Float)
     mass = db.Column(db.Float)
+    hair_color  = db.Column(db.String(250))
+    skin_color  = db.Column(db.String(250))
+    eye_color  = db.Column(db.String(250))
+    birth_year = db.Column(db.Integer)
+    gender = db.Column(db.String(250))
+    homeworld = db.Column(db.String(250))
     people_favorite = db.relationship("Favorite_People", backref="people")
 
+#Characters serialize
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "mass": self.mass          
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "homeworld": self.homeworld
         }
 
+# Tabla Pivote: Characters/ Favorites
 class Favorite_People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #con el nombre de la tabla user y atributo id
     people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
-    #Esta es una tabla pivote para relacionar User y People, relación muchos a muchos
+    #Esta es una tabla pivote para relacionar User y Characters, relación muchos a muchos
 
     def serialize(self):
         return {
             "id": self.id,
             "user_email": User.query.get(self.user_id).serialize()['email'],
             "character_name": People.query.get(self.people_id).serialize()['name']          
+        }
+
+
+# Tabla Planets
+class Planets (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    diameter = db.Column(db.Float)
+    rotation_Period = db.Column(db.Float)
+    orbital_Period = db.Column(db.Float)
+    gravity = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+    surface_Water = db.Column(db.Integer)
+    planets_favorite = db.relationship("Favorite_Planets", backref="planets")
+
+    #Planets serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_Period": self.rotation_Period,
+            "orbital_Period": self.orbital_Period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_Water": self.surface_Water
+        }
+
+# Tabla Pivote: Planets/ Favorites    
+#Esta es una tabla pivote para relacionar User y Planets, relación muchos a muchos
+class Favorite_Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #con el nombre de la tabla user y atributo id
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    #serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_email": User.query.get(self.user_id).serialize()['email'],
+            "planet_name": Planets.query.get(self.planet_id).serialize()['name'],
+        }
+
+class Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    model = db.Column(db.String(250))
+    vehicle_class = db.Column(db.String(250))
+    manufacturer = db.Column(db.String(250))
+    cost_in_credits = db.Column(db.Integer)
+    length = db.Column(db.Float)
+    crew = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+    max_atmosphering_speed = db.Column(db.Float)
+    cargo_capacity = db.Column(db.Float)
+    consumables = db.Column(db.String(250))
+    vehicles_favorite = db.relationship("Favorite_Vehicles", backref="vehicles")
+
+    #serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables
+        }
+
+# Tabla Pivote: Vehicles/ Favorites
+class Favorite_Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #con el nombre de la tabla user y atributo id
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    #Esta es una tabla pivote para relacionar User y Vehicles, relación muchos a muchos
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_email": User.query.get(self.user_id).serialize()['email'],
+            "vehicle_name": Vehicles.query.get(self.vehicles_id).serialize()['name']       
         }
